ec9962cf211148be1c70c3c14943fa986ce5faf8
venkataa315 <venkatdev@58@gmail.com>
2023-04-11 07:14:11 +0000

    vpc terraform file

---
+++ main.tf
@@ -0,0 +1,7 @@
+#setting-up provider
+
+provider "aws" {
+    region = "us-east-1"  
+}
+
+# write the ec2 instance config 
---
+++ variable.tf
@@ -0,0 +1,5 @@
+variable "vpc-cidr" {
+    default = "10.0.0.0/16"
+    type = string
+  
+}
